5fc77dee8b1037178e04e6dd2090694927c49c3d
josassi <johan.sassi@gmail.com>
2022-03-23 13:49:53 +0800

    Correct price info function masks

--- query_functions/get_functions.py
+++ query_functions/get_functions.py
@@ -7,13 +7,6 @@ from query_functions.etl_functions import *
 from query_functions.requests_functions import *
 
 URL = 'https://api.sorare.com/graphql'
-COMPETITIONS = ["bundesliga-de",
-                "ligue-1-fr",
-                "laliga-santander",
-                "serie-a-it"]
-PLAYER_SLUG_EXAMPLE = 'benjamin-pavard'
-EXPORT_PATH = "~/Documents/Code/Sorare/"
-CLUB_SLUG_EXAMPLE = "strasbourg-strasbourg"
 SLEEP = 2
 
 
@@ -78,7 +71,6 @@ def get_all_cards_from_player(player_slug,
                                                         end_cursor=end_cursor)}).json()
         has_next_page = r_page["data"]["player"]["cards"]["pageInfo"]["hasNextPage"]
         end_cursor = r_page["data"]["player"]["cards"]["pageInfo"]["endCursor"]
-
     if "node.latestEnglishAuction.endDate" in df_total.columns:
         df_total.loc[:, "node.latestEnglishAuction.endDate"] = pd.to_datetime(
             df_total["node.latestEnglishAuction.endDate"])
@@ -88,7 +80,7 @@ def get_all_cards_from_player(player_slug,
 
 def get_price_info_from_cards_df(df_cards: pd.DataFrame) -> (dict, pd.DataFrame):
     """
-    Function scrapping relevqnt price information from a cards dataframe.
+    Function scrapping relevant price information from a cards dataframe.
     :param df_cards: dataframe containing cards with liveSingleSaleOffer and latestEnglishAuction information
     :return: a dict with last_auction_price and lower_single_sale_offer, and the entry dataframe with an
     additional "%toLastAuctionPrice" column comparing the single sale offer to the latest auction price.
@@ -100,29 +92,38 @@ def get_price_info_from_cards_df(df_cards: pd.DataFrame) -> (dict, pd.DataFrame)
     now = pd.to_datetime(datetime.now().astimezone())
 
     if "node.liveSingleSaleOffer.price" in df_cards.columns:
-        df_cards["node.liveSingleSaleOffer.price"] = df_cards["node.liveSingleSaleOffer.price"].fillna(0).apply(lambda x:float(x))
-        price_dict["lower_single_sale_offer"] = df_cards["node.liveSingleSaleOffer.price"].min()
+        df_cards["node.liveSingleSaleOffer.price"] = df_cards["node.liveSingleSaleOffer.price"].fillna(0).apply(
+            lambda x: float(x))
+        mask_offer = df_cards["node.liveSingleSaleOffer.price"] > 0
+        price_dict["lower_single_sale_offer"] = df_cards[mask_offer]["node.liveSingleSaleOffer.price"].min()
 
     if "node.latestEnglishAuction.endDate" in df_cards.columns:
         df_cards["node.latestEnglishAuction.currentPrice"] = df_cards["node.latestEnglishAuction.currentPrice"].fillna(
             0).apply(lambda x: int(x))
         df_cards["node.latestEnglishAuction.endDate"] = pd.to_datetime(df_cards["node.latestEnglishAuction.endDate"])
 
-        mask_1 = (df_cards["node.latestEnglishAuction.open"]).fillna(True)
-        mask_2 = (df_cards["node.latestEnglishAuction.cancelled"]).fillna(True)
+        mask_1 = (df_cards["node.latestEnglishAuction.open"].fillna(False))
+        mask_2 = (df_cards["node.latestEnglishAuction.cancelled"].fillna(False))
         mask_3 = (df_cards["node.latestEnglishAuction.endDate"] <= now).fillna(True)
+        mask_4 = (df_cards["node.latestEnglishAuction.currentPrice"].fillna(0) > 0)
 
-        df_auction = df_cards[(~mask_1) & mask_2 & (~mask_3)]
+        df_auction = df_cards[(~mask_1) & (~mask_2) & mask_3 & mask_4]
         df_auction = df_auction.sort_values(by="node.latestEnglishAuction.endDate", ascending=False)
-        mask_4 = (df_auction["node.latestEnglishAuction.currentPrice"] > 0).fillna(True)
 
         if len(df_auction) > 0:
-            price_dict["last_auction_price"] = df_auction[mask_4]["node.latestEnglishAuction.currentPrice"].iloc[0]
+            price_dict["last_auction_price"] = df_auction["node.latestEnglishAuction.currentPrice"].iloc[0]
+            price_dict["last_auction_date"] = df_auction["node.latestEnglishAuction.endDate"].iloc[0]
             price_dict["max_auction_price"] = df_auction["node.latestEnglishAuction.currentPrice"].max()
             price_dict["average_auction_price"] = df_auction["node.latestEnglishAuction.currentPrice"].mean()
 
-    # df_cards.loc["%toLastAuctionPrice"] = (df_cards["node.liveSingleSaleOffer.price"]
-    #                                        - price_dict["last_auction_price"]) / price_dict["last_auction_price"]
-
     return price_dict
 
+
+if __name__ == "__main__":
+    PLAYER_SLUG_EXAMPLE = 'benjamin-pavard'
+    CLUB_SLUG_EXAMPLE = "strasbourg-strasbourg"
+    # EXPORT_PATH = "~/Documents/Code/Sorare/"
+    info = get_all_cards_from_player(player_slug="arber-zeneli")
+    print(info)
+    print(info[info["node.slug"] == "arber-zeneli-2021-limited-182"].iloc[0])
+    # f"arber-zeneli_cards_info.csv"
